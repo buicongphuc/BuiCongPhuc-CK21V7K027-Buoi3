83ef8c9fd8bd55117579afa5cf8e7a978e98159b
buicongphuc <buicongphuc191117@gmail.com>
2022-10-15 14:30:08 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -36,6 +36,16 @@
                 <i class="fas fa-address-card"></i>
             </h4>
             <ContactCard :contact="activeContact" />
+            <router-link
+                :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }"
+            >
+                <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                >
+            </router-link>
         </div>
     </div>
 </div>
@@ -91,7 +101,7 @@ methods: {
             console.log(error);
         }
     },
-    refreshList() {
+    async refreshList() {
         this.retrieveContacts();
         this.activeIndex = -1;
     },
@@ -105,7 +115,7 @@ methods: {
         }
     }
 },
-    goToAddContact() {
+    async goToAddContact() {
      this.$router.push({ name: "contact.add" });
     },
 },
